3cc689baec23fcdf48a3274d612c466d2e6133d7
Sabrippo <sabrinaippolito@hotmail.com.ar>
2023-09-17 19:20:37 -0300

    cambios 17/09/23

--- js/gatitos.js
+++ js/gatitos.js
@@ -1,75 +1,26 @@
+document.addEventListener('DOMContentLoaded', function() {
 const gatitos = [
     {
         nombre: 'Pippo',
         descripcion: 'Súper cariñoso y tranquilo',
-        raza: 'De la M',
-        edad: 'Rock',
-        vacunas: '2017-10-27',
-        castrado: 'Random',
-        imagen: 'https://example.com/imagenes/charly_garcia.jpg',
+        raza: 'Persa',
+        edad: '2 años',
+        vacunas: 'Trivalente',
+        castrado: 'Si',
+        imagen: 'https://.jpg',
         src: 'gatito1.html',
     },
     {
         nombre: 'Dobby',
         descripcion: 'Se lleva bien con otros gatos',
-        raza: 'Solista',
-        edad: 'Rock',
-        vacunas: '2017-10-27',
-        castrado: 'Random',
-        imagen: 'https://example.com/imagenes/charly_garcia.jpg',
+        raza: 'Siberiano',
+        edad: '6 años',
+        vacunas: 'Trivalente, leucemia',
+        castrado: 'Si',
+        imagen: 'https://.jpg',
         src: 'gatito2.html',
-    },
-    {
-        nombre: 'Frodo',
-        descripcion: 'Muy juguetón, no se queda quieto',
-        raza: 'Solista',
-        edad: 'Rock',
-        vacunas: '2017-10-27',
-        castrado: 'Random',
-        imagen: 'https://example.com/imagenes/charly_garcia.jpg'
-    },
+    },    
 ];
-// const section = document.querySelector('#container');  
-// function createList(gatito) {
-
-//     const {nombre, raza, edad, vacunas, castrado, imagen} = gatito;
-
-//     let card = document.createElement('div');
-//     card.classList.add('card');
-
-//     let nombreGatito = document.createElement('h2');
-//     let nombreTexto = document.createTextNode(nombre);
-//     nombreGatito.appendChild(nombreTexto);
-
-
-//     let razaGatito = document.createElement('h2');
-//     let razaTexto = document.createTextNode(raza);
-//     razaGatito.appendChild(razaTexto);
-
-//     let edadGatito = document.createElement('h2');
-//     let edadTexto = document.createTextNode(edad);
-//     edadGatito.appendChild(edadTexto);
-
-
-//     let vacunasGatito = document.createElement('h2');
-//     let vacunasTexto = document.createTextNode(vacunas);
-//     vacunasGatito.appendChild(vacunasTexto);
-
-//     let castradoGatito = document.createElement('h2');
-//     let castradoTexto = document.createTextNode(castrado);
-//     castradoGatito.appendChild(castradoTexto);
-
-//     let imagenGatito = document.createElement('img');
-//     imagenGatito.setAttribute('src', imagen);
-//     imagenGatito.setAttribute('alt', nombre);
-
-//     card.append(nombreGatito,razaGatito, edadGatito, vacunasGatito, castradoGatito, imagenGatito)
-//     section.appendChild(card);
-// }
-
-// gatitos.forEach( (gatito) => {
-//     createList(gatito);
-// })
 
 //ARMO LISTA DE GATITOS//
 const listaDeGatitos = document.querySelector("#listaDeGatitos");
@@ -80,16 +31,54 @@ const listadoGatitos = (gatitos) => {
       const li = document.createElement("li");
       li.innerHTML = `<span>${gatito.nombre}. ${gatito.descripcion}</span>`; 
       
-      const botonEditar = document.createElement("button");
-        botonEditar.innerHTML = "Ver gatito"; 
-        botonEditar.onclick = function() {
-            // Redirigir a gatito1.html
-            window.location.href = `${gatito.src}`;
+      const botonVer = document.createElement("button");
+        botonVer.innerHTML = "Ver gatito"; 
+        botonVer.onclick = function() {
+           
+            window.location.href = gatito.src;
         };        
         listaDeGatitos.appendChild(li);
-      li.appendChild(botonEditar);
+        li.appendChild(botonVer);
     });
 };
 listadoGatitos(gatitos);
 
+
+
+ //CREO CARD DE GATITOS//
+const infoGatito = document.querySelector("#infoGatito");
+const nombreGatito = window.location.pathname.split('/').pop().split('.')[0];
+const gatito = gatitos.find(g => g.src === `${nombreGatito}.html`);
+
+if(gatito) {
+  
+  const card = document.createElement("card");
+  const nombre = document.createElement("H1");
+  const raza = document.createElement("p");
+  const edad = document.createElement("p");
+  const vacunas = document.createElement("p");
+  const castrado = document.createElement("p");
+  const imagen = document.createElement("img");
+  const src = document.createElement("src");  
+  
+ nombre.textContent = gatito.nombre;
+ raza.textContent = gatito.raza;
+ edad.textContent = gatito.edad;
+ vacunas.textContent = gatito.vacunas;
+ castrado.textContent = gatito.castrado;
+ imagen.src = gatito.imagen;
+ 
+ 
+ card.appendChild(nombre);
+ card.appendChild(raza);
+ card.appendChild(edad);
+ card.appendChild(vacunas);
+ card.appendChild(castrado);
+ card.appendChild(imagen);
+  
+ infoGatito.appendChild(card);
+}else {
+    console.error(`No se encontró información para el gatito ${nombreGatito}`);
+}
+});
 
