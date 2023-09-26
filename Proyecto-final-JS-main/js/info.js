08ad0f42da76534ce817a42cbd8be7600c587a9b
Sabrippo <sabrinaippolito@hotmail.com.ar>
2023-09-26 20:48:30 -0300

    html 2 con botones listos

--- Proyecto-final-JS-main/js/info.js
+++ Proyecto-final-JS-main/js/info.js
@@ -16,10 +16,10 @@ document.addEventListener ("DOMContentLoaded", () => {
 
         const muestroGatito = document.createElement ("div");
         muestroGatito.classList.add ("esteGatito");
-        muestroGatito.classList.add("card", "d-flex", "flex-row","align-items-start");
+        muestroGatito.classList.add("card", "d-flex", "flex-column","mx-auto", "text-center", "border-0");
         muestroGatito.innerHTML = `
-        <img src="${gatitoElegido.imagen}" class="card-img" style="width: 50%;">
-        <div class="card-body"> 
+        <img src="${gatitoElegido.imagen}" class="card-img" style="width: 40%;">
+        <div class="card-body text-center mx-auto"> 
         <h2 class="card-title">Me llamo ${gatitoElegido.nombre}</h2>
         <p class="card-text">Tengo ${gatitoElegido.edad}</p>
         <p class="card-text">Soy un gatito ${gatitoElegido.raza}</p>
@@ -27,9 +27,17 @@ document.addEventListener ("DOMContentLoaded", () => {
         <p class="card-text">Estoy castrado? ${gatitoElegido.castrado}</p>
         </div>
         `;
-        contenedor.appendChild(muestroGatito);
-        
+        contenedor.appendChild(muestroGatito);          
     }
+    document.getElementById("pregunta").onclick = function() {
+        var email = prompt("Por favor, dejanos tu email");
     
+        if (email !== null && email.includes("@") && email.includes(".com")) {
+            localStorage.setItem("email", email);
+            alert("Muchas gracias!! Nos contactaremos a la brevedad.");
+        } else if (email !== null) {
+            alert("El correo electrónico ingresado no es válido.");
+        }
+    }
 
 });
